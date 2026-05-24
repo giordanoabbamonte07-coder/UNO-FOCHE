9ec9945c22009955778f68f5ac8c89862d68d3de
giordanoabbamonte07-coder <giordanoabbamonte07@gmail.com>
2026-05-24 23:31:02 +0200

    Delete 6.0 directory

--- 6.0/cmake-build-debug/CMakeFiles/uno_game.dir/audio.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/uno_game.dir/audio.c.o: \
- /home/giordanoabbamonte/CLionProjects/UNO-FOCHE/6.0/audio.c \
- /usr/include/stdc-predef.h \
- /home/giordanoabbamonte/CLionProjects/UNO-FOCHE/6.0/audio.h \
- /usr/include/raylib.h \
- /usr/lib/gcc/x86_64-redhat-linux/15/include/stdarg.h \
- /usr/lib/gcc/x86_64-redhat-linux/15/include/stdbool.h
